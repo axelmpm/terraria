7dde270c0431c14d78d05923362d6c6ded4039e3
Axel <axelmpm@gmail.com>
2020-03-16 00:31:00 -0300

    proyect

---
+++ cmake-build-debug/CMakeFiles/terraria.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/terraria.dir/main.cpp.o"
+  "terraria"
+  "terraria.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/terraria.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
